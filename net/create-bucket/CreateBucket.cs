db1c222ac44e7406f30bbc4164bf4ad1dbce1d11
Morgan Willis <willismt@amazon.com>
2022-11-21 17:44:19 +0000

    Renaming files and adding code for create bucket example for .net

---
+++ net/create-bucket/CreateBucket.cs
@@ -0,0 +1,32 @@
+using Amazon.S3;
+using Amazon;
+using Amazon.S3.Model;
+
+public class CreateBucket
+{
+    public static void Main(string[] args)
+    {
+        IAmazonS3 s3Client = new AmazonS3Client(RegionEndpoint.USEast1);
+        create(s3Client, "morgansamples3bucket-123456");
+    }
+
+    private static async void create(IAmazonS3 s3Client, string bucketName)
+    {
+        try
+        {
+            var putBucketRequest = new PutBucketRequest
+            {
+                BucketName = bucketName,
+                UseClientRegion = true,
+            };
+
+            var putBucketResponse = await s3Client.PutBucketAsync(putBucketRequest);
+
+        }
+        catch (AmazonS3Exception ex)
+        {
+            Console.WriteLine($"Error creating bucket: '{ex.Message}'");
+        }
+    }
+}
+
